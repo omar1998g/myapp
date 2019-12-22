867f5a54d8db79d116684b005a155a87f049a1a5
Omar González <omar.1998g@gmail.com>
2019-12-22 22:56:24 +0100

    Updates

---
+++ precache-manifest.0bf555047810a49a4d9c44dc0a4c641e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25abc7971c767c70a316b828c87f91c3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "70273bdb1352cd1136a2",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cc56ca433aa9add4c52e",
+    "url": "/my-app/static/js/2.36767d31.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.36767d31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70273bdb1352cd1136a2",
+    "url": "/my-app/static/js/main.f57aed63.chunk.js"
+  },
+  {
+    "revision": "8532c8ccf6313397fe41",
+    "url": "/my-app/static/js/runtime-main.7a91ef07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
